91a2c1c8756248ce0f5613ef6af260721edeacb0
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-01-12 19:14:25 +0300

    Temel kavramrı tartışıyoruz

---
+++ TemelKavramlar/TemelKavramlar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TemelKavramlar
+{
+
+
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+             int sayi =257;
+            sbyte negatif = -128;
+
+            checked
+            {
+                byte deger = (byte)sayi;
+                Console.WriteLine(deger);
+            }
+         
+
+        }
+    }
+}
