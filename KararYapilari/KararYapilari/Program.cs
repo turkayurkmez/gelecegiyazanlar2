604ad44dac16e91f619cfc767b89675bac05487a
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-01-12 22:09:40 +0300

    Karar mekanizmaları

---
+++ KararYapilari/KararYapilari/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace KararYapilari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int x = 8;
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("çifttir");
+            }
+            else
+            {
+                Console.WriteLine("tektir");
+            }
+
+            if (DateTime.Now.Month == 7 && DateTime.Now.Day == 1)
+            {
+                Console.WriteLine("Türkay'ın doğum günü");
+            }
+
+            string ay = string.Empty;
+            if (ay == "Aralık" || ay == "Ocak" || ay == "Şubat")
+            {
+                Console.WriteLine("Kış ayındasınız");
+            }
+
+
+            Console.WriteLine("Hangi renk trafik lambası yaniyor (K, S, Y)");
+            string renk = Console.ReadLine();
+            if (renk == "K")
+            {
+                Console.WriteLine("DUR!");
+            }
+            else if (renk == "S")
+            {
+                Console.WriteLine("Dikkat");
+            }
+            else if (renk == "Y")
+            {
+                Console.WriteLine("Geç");
+            }
+
+            switch (renk)
+            {
+                case "K":
+                    Console.WriteLine("DUR!");
+
+                    break;
+                case "S":
+                    Console.WriteLine("Dikkat");
+                    break;
+                case "Y":
+                    Console.WriteLine("Geç");
+                    break;
+                default:
+                    break;
+            }
+
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    break;
+                case DayOfWeek.Monday:
+                    break;
+                case DayOfWeek.Tuesday:
+                    break;
+                case DayOfWeek.Wednesday:
+                    break;
+                case DayOfWeek.Thursday:
+                    break;
+                case DayOfWeek.Friday:
+                    break;
+                case DayOfWeek.Saturday:
+                    break;
+                default:
+                    break;
+            }
+
+
+
+
+
+        }
+    }
+}
--- TemelKavramlar/TemelKavramlar/Program.cs
+++ TemelKavramlar/TemelKavramlar/Program.cs
@@ -6,18 +6,68 @@ namespace TemelKavramlar
 
     class Program
     {
-      
+
         static void Main(string[] args)
         {
-             int sayi =257;
-            sbyte negatif = -128;
+            //int sayi = 257;
+            //sbyte negatif = -128;
 
-            checked
+            //checked
+            //{
+            //    byte deger = (byte)sayi;
+            //    Console.WriteLine(deger);
+            //}
+
+            //int deger = 1000;
+            //long buyuk = deger;
+            //short kucuk = (short)deger;
+
+            Console.WriteLine("Kilonuzu giriniz:");
+            string girilenKilo = Console.ReadLine();
+
+            try
             {
-                byte deger = (byte)sayi;
-                Console.WriteLine(deger);
+                int kilo = Convert.ToInt32(girilenKilo);
+                Console.WriteLine("Boyunuzu metre olarak giriniz");
+                string girilenBoy = Console.ReadLine();
+                double boy = Convert.ToDouble(girilenBoy);
+
+                double bmi = kilo / (boy * boy);
+                if (double.IsInfinity(bmi))
+                {
+                    throw new Exception("boy 0 olduğundan bmi hesapnamadı");
+                }
+                else
+                {
+                    Console.WriteLine($"Vücut kitle endeksiniz: {bmi}");
+                }
+
             }
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Lütfen sayısal değer giriniz");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Tam sayılar 0'a bölünemez");
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            String s = "merhaba";
+            string x = "merhaba";
+            
+
+            //bool olurMu = true;
+            //Boolean olsunMu = false;
+            //int sayi = 5;
          
+
+
+
 
         }
     }
