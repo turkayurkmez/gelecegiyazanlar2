bf45c2e64f4de97ad497bcb2cd3707786b6c0ca0
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-01-24 20:47:35 +0300

    We started OOP concepts

---
+++ OOPOverview/ClassVSObject/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassVSObject
+{
+    public class Person
+    {
+        public string Name;
+        public string LastName;
+        public byte Age;
+        public bool IsMarried;
+        public double Weight;
+        public Address HomeAddress;
+        public Address WorkAddress;
+
+    }
+}
---
+++ OOPOverview/ClassVSObject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassVSObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int yas = 42;
+            bool olurMu = true;
+            if (yas==5)
+            {
+
+            }
+
+            Person worker = new Person();
+            worker.Name = "Murat";
+            worker.Age = 38;
+            worker.LastName = "Pirinççi";
+
+            Person cooker = worker;
+            cooker.Name = "Filiz";
+            cooker.Age = 24;
+
+
+
+            Console.WriteLine(worker.Name);
+        }
+    }
+}
