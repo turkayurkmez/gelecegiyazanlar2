891fc537a91e431e205a99dc25fd8850cf824054
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-02-23 20:55:56 +0300

    Orm

---
+++ ORM/introEntityFramework/introEntityFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace introEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
