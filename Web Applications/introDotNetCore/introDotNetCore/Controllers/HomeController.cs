f3f9c168299cca112138edfa5815ee6e7653f29c
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-03-21 22:10:58 +0300

    MVC Intro

---
+++ Web Applications/introDotNetCore/introDotNetCore/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using introDotNetCore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace introDotNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Age = 42;
+            ViewBag.Name = "Türkay";
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        } 
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks");
+            }
+           
+            return View();
+        }
+    }
+}
---
+++ Web Applications/introDotNetCore/introDotNetCore/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace introDotNetCore.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage ="Kullanıcı adı boş olamaz")]
+        [MinLength(2,ErrorMessage ="En az iki karakter olmalı")]
+        [Display(Name = "Kullanıcı adı")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Eposta boş olamaz")]
+        [EmailAddress(ErrorMessage ="Eposta formatı hatalı")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Doğum tarihi boş olamaz")]
+        public DateTime? BirthDate { get; set; }
+    }
+}
