8abdd7afb1b382fa31f18381ce7cfc65af4bb81a
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-04-18 20:20:47 +0300

    last check

---
+++ Web Applications/introApi/apiWithdotNetSix/apiWithdotNetSix/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiWithdotNetSix.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+                new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+                new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            };
+
+            return Ok(products);
+
+        }
+                
+           
+        
+    }
+}
