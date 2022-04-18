ac960e974842f19d5ff5ebab953925771320c09f
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-04-18 20:30:00 +0300

    nullable type changed

--- Web Applications/introApi/apiWithdotNetSix/apiWithdotNetSix/Controllers/Product.cs
+++ Web Applications/introApi/apiWithdotNetSix/apiWithdotNetSix/Controllers/Product.cs
@@ -3,7 +3,7 @@
     internal class Product
     {
         public int Id { get; internal set; }
-        public string Name { get; internal set; }
+        public string? Name { get; internal set; }
         public string Category { get; internal set; }
         public decimal Price { get; internal set; }
     }
